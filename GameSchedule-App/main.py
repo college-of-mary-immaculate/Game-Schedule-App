815b15b115f1b23f7c8a1ecc61143b9bebd8ceb9
Vince9090 <vincecarlodecastro5@gmail.com>
2024-06-08 18:44:09 +0800

    Main :recycle:

---
+++ GameSchedule-App/main.py
@@ -0,0 +1,6 @@
+from gamewikiapp import GameScheduleUI
+
+if __name__ == "__main__": #*Still Ongoing Project ,Analysis and Design Algorithm | 2023 - 2024 | Final Examination
+    app = GameScheduleUI()
+    app.run()
+    
